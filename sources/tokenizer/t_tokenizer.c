9bf7752ee13e6ab83c08c168cd7132d2df4188ec
Gillian Delvigne <gdelvign@iMac-de-Gillian.local>
2024-03-13 15:43:38 +0100

    to fix: number of tokens in every case, sucessions of quotes without space

--- sources/tokenizer/t_tokenizer.c
+++ sources/tokenizer/t_tokenizer.c
@@ -6,31 +6,61 @@
 /*   By: gdelvign <gdelvign@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 16:16:05 by gdelvign          #+#    #+#             */
-/*   Updated: 2024/03/13 12:13:05 by gdelvign         ###   ########.fr       */
+/*   Updated: 2024/03/13 15:38:58 by gdelvign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+
+bool	ft_is_quote(char c)
+{
+	if (c == DBL_Q || c == SGL_Q)
+		return (true);
+	return (false);
+}
+
 int	ft_count_tokens(char *input)
 {
 	int		count;
-	int		in_quotes;
 	int		quote_char;
-	char	*start_of_input;
+	char	*input_start;
 
 	count = 0;
-	in_quotes = false;
 	quote_char = 0;
-	start_of_input = input;
-	printf("tu es ici\n");
+	input_start = input;
 	while (*input)
 	{
-		
+		if (ft_is_quote(*input))
+		{
+			quote_char = *input;
+			if (input == input_start || *(input - 1) == SP)
+				count++;
+			input++;
+			while (*input && *input != quote_char)
+				input++;
+			if (*input == EOS)
+				return (-1);
+			if (*input == quote_char)
+			{
+				if (*(input + 1) == EOS)
+					input++;
+				else
+				{
+					quote_char = 0;
+					input++;
+				}
+			}
+		}
+		else if (*input != SP)
+		{
+			count++;
+			while (*input && *input != SP)
+				input++;
+		}
+		else
+			input++;
 	}
-	if (in_quotes)
-		return (-1);
-	printf("tu es là\n");
 	return (count);
 }
 
