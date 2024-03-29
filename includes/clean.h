f296325e88432495e473a4a43055de3c78d56228
Gillian Delvigne <gdelvign@iMac-de-Gillian.local>
2024-03-29 20:00:45 +0100

    minishell is basically functional with the new struct thanks to free functions

--- includes/clean.h
+++ includes/clean.h
@@ -6,7 +6,7 @@
 /*   By: gdelvign <gdelvign@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 16:32:04 by gdelvign          #+#    #+#             */
-/*   Updated: 2024/03/29 16:36:23 by gdelvign         ###   ########.fr       */
+/*   Updated: 2024/03/29 19:57:08 by gdelvign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 
 /* clean.c */
 void	ft_free_tokens(t_tok_lst **tokens);
+void	ft_reset_data(t_data *data);
 
 #endif
