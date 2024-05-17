27a64462e2e796f57d04c43ef5de748acc0648e3
JNTHNN <j.gasparoly@gmail.com>
2024-05-17 10:50:33 +0200

    fix exit

--- sources/builtins/exit.c
+++ sources/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: jgasparo <jgasparo@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 09:36:19 by jgasparo          #+#    #+#             */
-/*   Updated: 2024/05/13 19:20:54 by jgasparo         ###   ########.fr       */
+/*   Updated: 2024/05/16 19:07:16 by jgasparo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ int	ft_exit(t_data *data, t_cmd *cmd)
 
 	exit_status = (u_int8_t)g_exit_code;
 	flag = 0;
-	ft_putendl_fd("exit", STDERR_FILENO);
+	if (data->nb_of_cmds == 1)
+		ft_putendl_fd("exit", STDERR_FILENO);
 	if (cmd->args[1])
 	{
 		exit_status = ft_atol(cmd->args[1], &flag);
